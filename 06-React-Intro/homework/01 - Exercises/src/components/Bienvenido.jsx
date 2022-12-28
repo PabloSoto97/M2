28a7e9de39df85723149c1b556978b914948e1a1
pablo <pablo_mix_16@hotmail.com>
2022-12-28 19:30:55 -0300

    ejemplo

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -5,7 +5,7 @@ const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
-  // el código de tu componente acá
+
   return;
 }
 
